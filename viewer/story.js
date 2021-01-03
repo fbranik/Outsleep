e4109c6b9551b50954586a7898120cf41707c650
Fotis Branikas <fbran@FBMcBkPro.local>
2021-01-03 19:01:46 +0200

    second commit

---
+++ viewer/story.js
@@ -0,0 +1,384 @@
+var story = {
+ "docName": "outsleep",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "Outsleep",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "2E243D13-5DA8-4F68-9638-8797EB2F700A",
+   "groupID": "CAFC280A-A64A-441E-BF46-4A7244D18ED6",
+   "index": 0,
+   "image": "splash.png",
+   "image2x": "splash@2x.png",
+   "width": 411,
+   "height": 869,
+   "x": 113,
+   "y": 255,
+   "title": "splash",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 411,
+      "height": 869
+     },
+     "isParentFixed": false,
+     "page": 1,
+     "index": 0
+    }
+   ]
+  },
+  {
+   "id": "2D429353-ACCA-4E8F-8DC4-B3279234F22B",
+   "groupID": "CAFC280A-A64A-441E-BF46-4A7244D18ED6",
+   "index": 1,
+   "image": "main.png",
+   "image2x": "main@2x.png",
+   "width": 411,
+   "height": 869,
+   "x": 624,
+   "y": 245,
+   "title": "main",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 342,
+      "y": 36,
+      "width": 47,
+      "height": 31
+     },
+     "isParentFixed": false,
+     "page": 5,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 332,
+      "y": 137,
+      "width": 57,
+      "height": 28
+     },
+     "isParentFixed": false,
+     "page": 2,
+     "index": 1
+    },
+    {
+     "rect": {
+      "x": 332,
+      "y": 235,
+      "width": 56,
+      "height": 32
+     },
+     "isParentFixed": false,
+     "page": 3,
+     "index": 2
+    }
+   ]
+  },
+  {
+   "id": "97CA664A-2E8C-4FAF-A8FD-95E675572C58",
+   "groupID": "CAFC280A-A64A-441E-BF46-4A7244D18ED6",
+   "index": 2,
+   "image": "weekdays_on.png",
+   "image2x": "weekdays_on@2x.png",
+   "width": 411,
+   "height": 869,
+   "x": 1135,
+   "y": -413,
+   "title": "weekdays on",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 342,
+      "y": 36,
+      "width": 47,
+      "height": 31
+     },
+     "isParentFixed": false,
+     "page": 5,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 332,
+      "y": 137,
+      "width": 57,
+      "height": 28
+     },
+     "isParentFixed": false,
+     "page": 1,
+     "index": 1
+    },
+    {
+     "rect": {
+      "x": 332,
+      "y": 235,
+      "width": 56,
+      "height": 32
+     },
+     "isParentFixed": false,
+     "page": 4,
+     "index": 2
+    }
+   ]
+  },
+  {
+   "id": "FEED416C-B5AE-410E-8798-405B92117F41",
+   "groupID": "CAFC280A-A64A-441E-BF46-4A7244D18ED6",
+   "index": 3,
+   "image": "weekends_on.png",
+   "image2x": "weekends_on@2x.png",
+   "width": 411,
+   "height": 869,
+   "x": 1135,
+   "y": 617,
+   "title": "weekends on",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 342,
+      "y": 38,
+      "width": 47,
+      "height": 31
+     },
+     "isParentFixed": false,
+     "page": 5,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 332,
+      "y": 235,
+      "width": 56,
+      "height": 32
+     },
+     "isParentFixed": false,
+     "page": 1,
+     "index": 1
+    },
+    {
+     "rect": {
+      "x": 332,
+      "y": 137,
+      "width": 57,
+      "height": 28
+     },
+     "isParentFixed": false,
+     "page": 4,
+     "index": 2
+    }
+   ]
+  },
+  {
+   "id": "1447D72E-547D-463C-B694-3AE6E3869A60",
+   "groupID": "CAFC280A-A64A-441E-BF46-4A7244D18ED6",
+   "index": 4,
+   "image": "both_on.png",
+   "image2x": "both_on@2x.png",
+   "width": 411,
+   "height": 869,
+   "x": 1811,
+   "y": 157,
+   "title": "both on",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 342,
+      "y": 36,
+      "width": 47,
+      "height": 31
+     },
+     "isParentFixed": false,
+     "page": 5,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 332,
+      "y": 137,
+      "width": 57,
+      "height": 28
+     },
+     "isParentFixed": false,
+     "page": 3,
+     "index": 1
+    },
+    {
+     "rect": {
+      "x": 332,
+      "y": 235,
+      "width": 56,
+      "height": 32
+     },
+     "isParentFixed": false,
+     "page": 2,
+     "index": 2
+    }
+   ]
+  },
+  {
+   "id": "07DE65E0-BA67-4209-922B-0FE70FE76933",
+   "groupID": "CAFC280A-A64A-441E-BF46-4A7244D18ED6",
+   "index": 5,
+   "image": "profiles.png",
+   "image2x": "profiles@2x.png",
+   "width": 411,
+   "height": 869,
+   "x": 1811,
+   "y": 1173,
+   "title": "profiles",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 82.79854368932047,
+      "y": 836,
+      "width": 14.96359223300965,
+      "height": 17
+     },
+     "isParentFixed": false,
+     "page": 1,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 20,
+      "y": 59,
+      "width": 16,
+      "height": 16
+     },
+     "isParentFixed": false,
+     "page": 1,
+     "index": 1
+    },
+    {
+     "rect": {
+      "x": 261,
+      "y": 51,
+      "width": 137,
+      "height": 32
+     },
+     "isParentFixed": false,
+     "page": 6,
+     "index": 2
+    }
+   ]
+  },
+  {
+   "id": "7F6E9E73-D1A0-49C1-B84B-5F4B70C9D1DA",
+   "groupID": "CAFC280A-A64A-441E-BF46-4A7244D18ED6",
+   "index": 6,
+   "image": "new_prof.png",
+   "image2x": "new_prof@2x.png",
+   "width": 411,
+   "height": 869,
+   "x": 2322,
+   "y": 1173,
+   "title": "new prof",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 76,
+      "y": 251,
+      "width": 23,
+      "height": 19
+     },
+     "isParentFixed": false,
+     "page": 5,
+     "index": 0
+    }
+   ]
+  }
+ ],
+ "groups": [
+  {
+   "id": "CAFC280A-A64A-441E-BF46-4A7244D18ED6",
+   "name": "Page 1"
+  },
+  {
+   "id": "72A45814-2ACA-426A-B9F9-3C3864A0CEA3",
+   "name": "Symbols"
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 7
+}
